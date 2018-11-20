bbb60263898a0544167187c27f05e2a192d1fa80
James Gromis <dagwoodsandwich56@gmail.com>
2018-11-20 19:03:55 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,15 +5,17 @@ using namespace std;
 
 int main()
 {
-  cfi.cbSize = sizeof(cfi);
+CONSOLE_FONT_INFOEX cfi;
+cfi.cbSize = sizeof(cfi);
 cfi.nFont = 0;
 cfi.dwFontSize.X = 0;                   // Width of each character in the font
 cfi.dwFontSize.Y = 24;                  // Height
 cfi.FontFamily = FF_DONTCARE;
 cfi.FontWeight = FW_NORMAL;
-std::wcscpy(cfi.FaceName, L"Times New Roman"); // Choose your font
+std::wcscpy(cfi.FaceName, L"Consolas"); // Choose your font
 SetCurrentConsoleFontEx(GetStdHandle(STD_OUTPUT_HANDLE), FALSE, &cfi);
 
+std::cout << "Font: Consolas, Size: 24\n";
 char a;
 int b;
 cout<< "Hamlet. exe:   A c++ mapped, console version of Hamlet. \n";
